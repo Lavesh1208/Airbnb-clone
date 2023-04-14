4675cafe2f47f677b40f17d247321a4c3958e6c5
Lavesh1208 <laveshs1282@gmail.com>
2023-04-14 12:10:16 +0530

    Signup and Login using Google and Github

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,11 +7,11 @@ import Avatar from '../Avatar'
 import MenuItem from './MenuItem'
 import useRegisterModal from '@/app/hooks/userRegisterModal'
 import useLoginModal from '@/app/hooks/userLoginModal'
-import { User } from '@prisma/client'
 import { signOut } from 'next-auth/react'
+import { SafeUser } from '@/app/types'
 
 interface UserMenuProps {
-   currentUser?: User | null
+   currentUser?: SafeUser | null
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({
@@ -40,7 +40,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
             >
                <AiOutlineMenu />
                <div className="hidden md:block">
-                  <Avatar />
+                  <Avatar src={currentUser?.image} />
                </div>
             </div>
          </div>
--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-  },
+   },
+   images: {
+      domains: [
+         "avatars.githubusercontent.com",
+         "lh3.googleusercontent.com"
+      ]
+   }
 }
 
 module.exports = nextConfig
